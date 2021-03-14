5254176601d7cde39ec71a9f2fe2f9fefebbf73e
Roin Kuntchulia <foxmr88@gmail.com>
2021-03-14 13:50:35 +0400

    first

---
+++ js/lib.js
@@ -0,0 +1,24 @@
+var tabclass = document.getElementsByClassName ("contacttab");
+function contacttabfun (tabid){
+	for (var i=0; i<tabclass.length; i++){
+		tabclass[i].style.display = 'none';
+	}
+	document.getElementById (tabid).style.display = 'block';
+}
+var menuBorder = document.getElementById ('mobile-menu');
+var menuBurger = document.getElementById ('burger');
+var k = 0;
+function mobileMenu (){
+	
+	if (k == 0) {
+		menuBorder.style.display = 'block';
+		menuBurger.style.opacity = '0';
+		k++;
+	}
+	else{
+		menuBorder.style.display = 'none';
+		menuBurger.style.opacity = '1';
+		k--;
+	}
+}
+menuBorder.innerHTML += document.getElementById ('header-nav').innerHTML;
